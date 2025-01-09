0154fa07b2c1f462f172182091a21473e3a47e4e
ums1212 <voll1212@gmail.com>
2025-01-10 00:46:11 +0900

    사용하지 않는 model 삭제

--- movie_recommendation/models.py
+++ movie_recommendation/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Recommendation(models.Model):
-    title = models.MovieTitle
-    year = models.IntegerField(default=0)
-    genre = models.TextField(default='')
-    duration = models.IntegerField(default=0)
-    director = models.CharField(max_length=200)
-    mainCast = models.TextField(default='')
-    recommendationReason = models.TextField(default='')
-    predicateScore = models.IntegerField(default=1)
-
-
-class MovieTitle(models.Model):
-    original = models.CharField(max_length=200)
-    korean = models.CharField(max_length=200)
