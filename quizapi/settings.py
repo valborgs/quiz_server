196eca2ac093e17afc3b8dcc95789d07d44b956c
ums1212 <voll1212@gmail.com>
2025-01-14 15:30:26 +0900

    MEDIA_ROOT 경로 수정

--- quizapi/settings.py
+++ quizapi/settings.py
@@ -135,5 +135,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
